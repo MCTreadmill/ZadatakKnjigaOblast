2b9c8f4a411ef79bd7a86e2f651d71f1c9b37e05
Luka  Stojic <luka@arthaus.info>
2017-10-09 16:56:44 +0200

    Uradjen zadatak brisanja vrednosti fixes #9

--- src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -1,17 +1,37 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Oblast;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Zadatak4BrisanjeVrednosti {
+    static Dao<Oblast,Integer> oblastDao;
+
     public static void main(String[] args) {
         ConnectionSource connectionSource = null;
 
         try {
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:knjigaOblast.db");
+
+            oblastDao = DaoManager.createDao(connectionSource, Oblast.class);
+
+            List<Oblast> oblasti = oblastDao.queryForAll();
+            for (Oblast o : oblasti)
+                System.out.println(o);
+
+            List<Oblast> oblastZaBrisanje = oblastDao.queryForEq(Oblast.POLJE_NAZIV, "Aritmeticki operatori");
+            Oblast oblast1 = oblastZaBrisanje.get(0);
+            oblastDao.delete(oblast1);
+
+            List<Oblast> oblasti2 = oblastDao.queryForAll();
+            for (Oblast o : oblasti2)
+                System.out.println(o);
 
 
         } catch (SQLException e) {
